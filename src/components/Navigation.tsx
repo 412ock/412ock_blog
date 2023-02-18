fa571d1678fad17642b04b123f43993a03f741fb
412ock <412ock.dev@gmail.com>
2023-02-18 22:42:11 +0900

    feat: Navigation Bar add Container CSS

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,12 +6,12 @@ export default function Navigation() {
         <nav>
             <div className={style.navbarWrapper}>
                 <div className={`${style.container}`}>
-                    <Link className={style.logo} href={`/`}>412ock&apos; Blog</Link>
+                    <Link className={style.logo} href={`/`}>412ock&apos; Blog</Link>    
+                    <ul className={style.navGroup}>
+                        <li className={style.navItem}><Link  href={`/about`}>About</Link></li>
+                        <li className={style.navItem}><Link  href={`/posts`}>Blog</Link></li>
+                    </ul>
                 </div>
-                <ul className={style.navGroup}>
-                    <li className={style.navItem}><Link  href={`/about`}>About</Link></li>
-                    <li className={style.navItem}><Link  href={`/posts`}>Blog</Link></li>
-                </ul>
             </div>
         </nav>
     )
