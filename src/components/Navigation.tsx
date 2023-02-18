b219cb9adb43dcc6349265f422c553bdbda6bc47
412ock <412ock.dev@gmail.com>
2023-02-18 11:31:30 +0900

    fix: href 따옴표 => ``로 변경

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,11 +6,11 @@ export default function Navigation() {
         <nav>
             <div className={style.navbarWrapper}>
                 <div className={`${style.container}`}>
-                    <Link className={style.logo} href='/'>412ock' Blog</Link>
+                    <Link className={style.logo} href={`/`}>412ock' Blog</Link>
                 </div>
                 <ul className={style.navGroup}>
-                    <li className={style.navItem}><Link  href='/about'>About</Link></li>
-                    <li className={style.navItem}><Link  href='/posts'>Blog</Link></li>
+                    <li className={style.navItem}><Link  href={`/about`}>About</Link></li>
+                    <li className={style.navItem}><Link  href={`/posts`}>Blog</Link></li>
                 </ul>
             </div>
         </nav>
