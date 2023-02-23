f61f84735a540a0210af11c0228b903cd0a6d196
412ock <412ock.dev@gmail.com>
2023-02-23 12:29:28 +0900

    fix: remark => unified 변경

--- src/lib/post.tsx
+++ src/lib/post.tsx
@@ -1,9 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import {remark} from 'remark';
-import remarkHtml from 'remark-html';
+
+// Unified Ecosystem
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
 import remarkGfm from 'remark-gfm';
+import remark2rehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import rehypeHighlight from 'rehype-highlight';
+
 
 const postsDirectory = path.join(process.cwd(), 'src', 'posts');
 
@@ -52,11 +58,16 @@ export async function getPostData(id: string) {
 
     const {data: {title, date}, content: content} = matter(fileContents);
 
-    const processedContent = await remark()
+    const processedContent = await unified()
+        .use(remarkParse)
         .use(remarkGfm)
-        .use(remarkHtml)
+        .use(remark2rehype)
+        .use(rehypeStringify)
+        .use(rehypeHighlight)
         .process(content);
     
+    
+
     const contentHtml = processedContent.toString();
 
     return {
---
+++ src/posts/codeblock.md
@@ -0,0 +1,14 @@
+---
+title: 'Remark 코드블럭 테스트'
+date: '2023-02-23'
+---
+
+```js
+const fruit1 = 'apple';
+const fruit2 = 'banana';
+
+const objFruits = {
+    fruit1,
+    fruit2,
+}
+```
